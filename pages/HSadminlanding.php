75a66628963df27dd37cf8454b926d4e3e32a069
Iamnotk3v <164435583+Iamnotk3v@users.noreply.github.com>
2024-04-04 11:33:55 -0500

    Update HSadminlanding.php

--- pages/HSadminlanding.php
+++ pages/HSadminlanding.php
@@ -4,25 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrator</title>
-    <style>
-        .welcome-message {
-            font-size: 24px;
-            margin-top: 50px;
-        }
-        .button-container {
-            margin-top: 20px;
-        }
-        .button {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #0074D9;
-            color: #ffffff;
-            text-decoration: none;
-            border-radius: 5px;
-            margin: 10px;
-            font-weight: bold;
-        }
-    </style>
 </head>
 <body>
     <div class="welcome-message">
