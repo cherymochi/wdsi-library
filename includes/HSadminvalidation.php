6cf56df90ca9009536569d07bb15ea8c9837e4f8
Iamnotk3v <164435583+Iamnotk3v@users.noreply.github.com>
2024-04-04 12:22:27 -0500

    Create HSadminvalidation.php

---
+++ includes/HSadminvalidation.php
@@ -0,0 +1,26 @@
+    <?php
+        // Check if form is submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = "";//I also don't know what to put here
+            $password = "";
+            
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            
+            if (empty($username) || empty($password)) {
+                echo "<script>alert('ERROR :  Please fill in all fields');</script>";
+
+                header("Location: HSadminsign.php");
+            }
+            
+            
+
+            // Check if username and password are correct
+            if ($_POST["username"] == $username && $_POST["password"] == $password) {
+                //Redirect to librarian dashboard
+                header("Location: ");
+            } else {
+                echo "<script>alert('ERROR :  Invalid Username or Password');</script>";
+            }
+        }
+    ?>
